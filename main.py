34c217212d4de41650796b0acc6030422e0d9b54
Anupama Sbk <ankh77sb@gmail.com>
2021-07-23 11:15:10 +0530

    fixed

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
 
-    from flask import Flask, render_template, request, redirect, url_for, session
-    app = Flask(__name__)
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
 
-    @app.route('/')
-    @app.route('/home')
-    def home():
-        return render_template("index.html")
-
-    app.run(debug=True )
+app.run(debug=True )
